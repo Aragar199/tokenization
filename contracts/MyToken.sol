690cd64687ed59c8ba5e8a80d86b362a60f88419
Aragar199 <avponce@hotmail.com>
2021-07-22 20:13:03 -0700

    add files

---
+++ contracts/MyToken.sol
@@ -0,0 +1,12 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) public ERC20("StarDucks Cappucino Token", "CAPPU") {
+        _setupDecimals(0);
+        _mint(msg.sender, initialSupply);
+    }
+}
